89a8e5fe48d2beb7b8d7d04657906edc591ec9a5
korigamik-hypr <korigamik@gmail.com>
2024-10-13 00:08:48 +0530

    feat(favorite): adds ability to add to favorites #10

--- src/provider/mod.rs
+++ src/provider/mod.rs
@@ -15,6 +15,18 @@ pub trait Provider {
 
 pub trait OnlineProvider: Provider {
     async fn authenticate(&self) -> Result<Secrets, Box<dyn std::error::Error>>;
-    async fn fetch_items(&self) -> Result<Vec<Self::Item>, Box<dyn std::error::Error>>;
-    async fn add_item(&self, uri: &str, tags: Vec<&str>) -> Result<Option<i64>, Box<dyn std::error::Error>>;
+    async fn fetch_items(
+        &self,
+        limit: Option<usize>,
+    ) -> Result<Vec<Self::Item>, Box<dyn std::error::Error>>;
+    async fn add_item(
+        &self,
+        uri: &str,
+        tags: Vec<&str>,
+    ) -> Result<Option<i64>, Box<dyn std::error::Error>>;
+    async fn mark_as_favorite(
+        &self,
+        item_id: i64,
+        mark: bool,
+    ) -> Result<(), Box<dyn std::error::Error>>;
 }
